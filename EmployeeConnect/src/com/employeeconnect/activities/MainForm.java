cf33cf5021fe3e3732c22a60436b4b425a637a05
priyeshsaxena <priyesh.saxena@cybercom.com>
2014-02-21 14:16:13 +0100

    Added Image

--- EmployeeConnect/src/com/employeeconnect/activities/MainForm.java
+++ EmployeeConnect/src/com/employeeconnect/activities/MainForm.java
@@ -16,6 +16,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -34,6 +35,8 @@ public class MainForm extends Activity {
 	public static final String EXTRA_MESSAGE4 = "com.example.demoform.MESSAGE4";
 	
 	EditText editText;
+	TextView textView;
+	ImageView image;
 
 	
 	
@@ -50,6 +53,8 @@ public class MainForm extends Activity {
 		ArrayList<String> name = new ArrayList<String>();
 		ArrayList<String> value = new ArrayList<String>();
 		Map<String,String> map = new LinkedHashMap<String, String>();
+		textView = new TextView(this);
+		
 		try {
 			while((test = br.readLine()) != null){
 				String[] test1 = test.split(":");
@@ -65,88 +70,66 @@ public class MainForm extends Activity {
 		
 	}
 	
-	public void showData(ArrayList<String> name, ArrayList<String> value) {
-		
-		LinearLayout layout = new LinearLayout(this);
-		for(int i = 0; i <name.size(); i++){
-			TextView textView = new TextView(this);
-			textView.setId(i);
-			textView.setText(name.get(i).toString() +":");
-			layout.addView(textView);
-		}
-		for(int i = 0; i <value.size(); i++){
-			EditText editView = new EditText(this);
-			editView.setId(i);
-			editView.setText(value.get(i).toString());
-			layout.addView(editView);
-		}
-		layout.setOrientation(LinearLayout.VERTICAL);
-		layout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-		setContentView(layout);
-		
-	}
+//	public void showData(ArrayList<String> name, ArrayList<String> value) {
+//		
+//		LinearLayout layout = new LinearLayout(this);
+//		for(int i = 0; i <name.size(); i++){
+//			TextView textView = new TextView(this);
+//			textView.setId(i);
+//			textView.setText(name.get(i).toString() +":");
+//			layout.addView(textView);
+//		}
+//		for(int i = 0; i <value.size(); i++){
+//			EditText editView = new EditText(this);
+//			
+//			editView.setId(i);
+//			editView.setText(value.get(i).toString());
+//			layout.addView(editView);
+//		}
+//		layout.setOrientation(LinearLayout.VERTICAL);
+//		layout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+//		setContentView(layout);
+//		
+//	}
 	
-	public void showData(Map<String,String> map){
+	public void showData(Map<String,String> map){		
 		
+		image = (ImageView) findViewById(R.id.imageView1);
+		image.setImageResource(R.drawable.android);
 		
 		Set<Entry<String, String>> testMaps = map.entrySet();
 		for (Entry<String, String> entry : testMaps) {
 			if(entry.getKey().equalsIgnoreCase("First_Name")){
-				editText = (EditText)findViewById(R.id.editFname);
-				editText.setText(entry.getValue());
+				textView = (TextView)findViewById(R.id.editFname);
+				textView.setText(entry.getValue());
+				
 			}else if(entry.getKey().equalsIgnoreCase("Middle_Name")){
-				editText = (EditText)findViewById(R.id.editMname);
-				editText.setText(entry.getValue());
+				textView = (TextView)findViewById(R.id.editMname);
+				textView.setText(entry.getValue());
+				
 			}else if(entry.getKey().equalsIgnoreCase("Last_Name")){
-				editText = (EditText)findViewById(R.id.editLname);
-				editText.setText(entry.getValue());
+				textView = (TextView)findViewById(R.id.editLname);
+				textView.setText(entry.getValue());
+				
 			}else if(entry.getKey().equalsIgnoreCase("Emp_Code")){
-				editText = (EditText)findViewById(R.id.editEmpCode);
-				editText.setText(entry.getValue());
+				textView = (TextView)findViewById(R.id.editEmpCode);
+				textView.setText(entry.getValue());
 			}else if(entry.getKey().equalsIgnoreCase("Phone")){
-				editText = (EditText)findViewById(R.id.editPhone);
-				editText.setText(entry.getValue());
+				textView = (TextView)findViewById(R.id.editPhone);
+				textView.setText(entry.getValue());
 			}else if(entry.getKey().equalsIgnoreCase("Email")){
-				editText = (EditText)findViewById(R.id.editEmail);
-				editText.setText(entry.getValue());
+				textView = (TextView)findViewById(R.id.editEmail);
+				textView.setText(entry.getValue());
 			}else if(entry.getKey().equalsIgnoreCase("DOB")){
-				editText = (EditText)findViewById(R.id.editDOB);
-				editText.setText(entry.getValue());
+				textView = (TextView)findViewById(R.id.editDOB);
+				textView.setText(entry.getValue());
 			}else if(entry.getKey().equalsIgnoreCase("Blood_Group")){
-				editText = (EditText)findViewById(R.id.editBloodGroup);
-				editText.setText(entry.getValue());
+				textView = (TextView)findViewById(R.id.editBloodGroup);
+				textView.setText(entry.getValue());
 			}
+			textView.setEnabled(false);
 		}
-		/*for(int i = 0; i<map.size(); i++){
-			if(map.containsKey("First Name")){
-				editText = (EditText)findViewById(R.id.editFname);
-				Log.i("Test ", map.get("First Name"));
-				editText.setText(map.get("First Name"));
-			}else if(map.containsKey("Middle Name")){
-				editText = (EditText)findViewById(R.id.editMname);
-				editText.setText(map.get("Middle Name"));
-			}else if(map.containsKey("Last Name")){
-				editText = (EditText)findViewById(R.id.editLname);
-				editText.setText(map.get("Last Name"));
-			}else if(map.containsKey("Emp Code")){
-				editText = (EditText)findViewById(R.id.editEmpCode);
-				editText.setText(map.get("Emp Code"));
-			}else if(map.containsKey("Phone")){
-				editText = (EditText)findViewById(R.id.editPhone);
-				editText.setText(map.get("Phone"));
-			}else if(map.containsKey("Email")){
-				editText = (EditText)findViewById(R.id.editEmail);
-				editText.setText(map.get("Email"));
-			}else if(map.containsKey("DOB")){
-				editText = (EditText)findViewById(R.id.editDOB);
-				editText.setText(map.get("DOB"));
-			}else if(map.containsKey("Blood Group")){
-				editText = (EditText)findViewById(R.id.editBloodGroup);
-				editText.setText(map.get("Blood Group"));
-			}
-		}
-		*/
-		
+				
 	}
 
 	@Override
